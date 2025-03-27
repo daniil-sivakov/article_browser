d73c2d36a8bbcf9dfe557be917e70763af311cf2
Daniil Sivakov <daniil.sivakov.work@gmail.com>
2025-03-27 18:15:55 +0300

    feat: update main.dart configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,37 @@
+import 'dart:async';
+
+import 'package:article_browser/core/di/core_module.dart';
+import 'package:article_browser/core/logging/logger.dart';
+import 'package:article_browser/data/di/data_module.dart';
+import 'package:article_browser/domain/di/domain_module.dart';
+import 'package:article_browser/presentation/app.dart';
+import 'package:cherrypick/cherrypick.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MainApp());
-}
+  final Scope rootScope = CherryPick.openRootScope();
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+  try {
+    rootScope.installModules([CoreModule()]);
+  } on Object {
+    runApp(Center(child: FlutterLogo()));
+    rethrow;
   }
+
+  runZonedGuarded(
+    () {
+      rootScope.installModules([DataModule(), DomainModule()]);
+
+      WidgetsFlutterBinding.ensureInitialized();
+
+      SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+        DeviceOrientation.portraitUp,
+      ]);
+      runApp(const App());
+    },
+    (error, stackTrace) {
+      rootScope.resolve<Logger>().handleError(error, stackTrace);
+    },
+  );
 }
